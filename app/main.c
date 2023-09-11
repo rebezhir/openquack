de80b4ed340928282dd64aa5cbc5861d7cba3b5f
Dmitry <masstarassta@gmail.com>
2023-09-11 07:48:55 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -235,7 +235,7 @@ static void MAIN_Key_DIGITS(KEY_Code_t Key, bool bKeyPressed, bool bKeyHeld) {
 static void MAIN_Key_EXIT(bool bKeyPressed, bool bKeyHeld) {
     if (!bKeyHeld && bKeyPressed) {
         gBeepToPlay = BEEP_1KHZ_60MS_OPTIONAL;
-        if (true) {
+        
             if (gScanState == SCAN_OFF) {
                 if (gInputBoxIndex == 0) {
                     return;
@@ -251,8 +251,8 @@ static void MAIN_Key_EXIT(bool bKeyPressed, bool bKeyHeld) {
             }
             gRequestDisplayScreen = DISPLAY_MAIN;
             return;
-        }
-        ACTION_FM();
+        
+        
     }
 }
 
